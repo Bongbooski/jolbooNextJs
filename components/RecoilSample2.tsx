655a9a9f1d7991855501d9f5380c71e941b8b4a6
booski <p9346420@gmail.com>
2023-03-17 15:29:25 +0900

    build error fix3

--- components/RecoilSample2.tsx
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from "recoil";
-import { SampleState } from "../state/SampleState";
-
-const RecoilSample2 = () => {
-  const isMarriedValue = useRecoilValue(SampleState.isMarriedValue);
-  return <div>{isMarriedValue ? "no!" : "ok!"}</div>;
-};
-
-export default RecoilSample2;
