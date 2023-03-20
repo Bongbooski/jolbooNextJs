f5385f0e96abe586b41bbd191af40302c70e91e0
booski <p9346420@gmail.com>
2023-03-20 21:07:22 +0900

    dynamic page 주석처리

--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,29 +1,29 @@
 import { useRouter } from "next/router";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import Seo from "../../components/Seo";
+// import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+// import Seo from "../../components/Seo";
 
-const Movie = ({ params }: InferGetServerSidePropsType<GetServerSideProps>) => {
-  const router = useRouter();
-  const [title, id] = params || [];
+// const Movie = ({ params }: InferGetServerSidePropsType<GetServerSideProps>) => {
+//   const router = useRouter();
+//   const [title, id] = params || [];
 
-  console.log("params:", params);
-  return (
-    <div>
-      <Seo title={title} />
-      <h4>{title}</h4>
-    </div>
-  );
-};
+//   console.log("params:", params);
+//   return (
+//     <div>
+//       <Seo title={title} />
+//       <h4>{title}</h4>
+//     </div>
+//   );
+// };
 
-export default Movie;
+// export default Movie;
 
-export const getServerSideProps = async ({
-  params: { params },
-}: InferGetServerSidePropsType<GetServerSideProps>) => {
-  // server context에서 params를 가져다가 화면에 전달하는 과정
-  return {
-    props: {
-      params,
-    },
-  };
-};
+// export const getServerSideProps = async ({
+//   params: { params },
+// }: InferGetServerSidePropsType<GetServerSideProps>) => {
+//   // server context에서 params를 가져다가 화면에 전달하는 과정
+//   return {
+//     props: {
+//       params,
+//     },
+//   };
+// };
--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -1,23 +1,23 @@
 import { useRouter } from "next/router";
 
-const Movie = () => {
-  const router = useRouter();
-  return (
-    <div>
-      <h4>{router.query.title || "Loading..."}</h4>
-    </div>
-  );
-};
+// const Movie = () => {
+//   const router = useRouter();
+//   return (
+//     <div>
+//       <h4>{router.query.title || "Loading..."}</h4>
+//     </div>
+//   );
+// };
 
-export default Movie;
+// export default Movie;
 
-export const getServerSideProps = async () => {
-  const { results } = await (
-    await fetch("http://localhost:3000/api/movies")
-  ).json();
-  return {
-    props: {
-      results,
-    },
-  };
-};
+// export const getServerSideProps = async () => {
+//   const { results } = await (
+//     await fetch("http://localhost:3000/api/movies")
+//   ).json();
+//   return {
+//     props: {
+//       results,
+//     },
+//   };
+// };
