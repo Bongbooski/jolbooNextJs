08d0774359793735ad5eb0034e180e2910d9ff46
woo <dnqudtjs@gmail.com>
2023-05-08 21:49:33 +0900

    google analytics 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import * as ga from "../lib/ga/gtag";
 import { RecoilRoot } from "recoil";
 import CssBaseline from "@mui/material/CssBaseline";
 import { MuiThemeProvider } from "../context/ThemeProvider";
 import AppLayout from "../components/layout/AppLayout";
 import { NextPage } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 /*For Per-page layout! */
 export type NextPageWithLayout = NextPage & {
@@ -17,6 +20,20 @@ type AppPropsWithLayout = AppProps & {
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout || ((page) => page);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      ga.pageView(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <MuiThemeProvider>
       <RecoilRoot>
