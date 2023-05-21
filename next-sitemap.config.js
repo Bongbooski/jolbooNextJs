ee1e96cc730b9312cd192752c8b2618477a6b0d2
woo <dnqudtjs@gmail.com>
2023-05-21 22:05:40 +0900

    sitemap 생성 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://letsbejolboos.com/",
+  generateRobotsTxt: true,
+  exclude: ["/admin", "/login"],
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 1.0,
+};
